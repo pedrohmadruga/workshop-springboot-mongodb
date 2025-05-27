2f80baa3b62ab6a175948c5e4ccdd5125936d6c9
Pedro Madruga <pedrohfonseca01@gmail.com>
2025-05-27 10:51:33 -0300

    Endpoint to return User posts

--- src/main/java/com/nelioalves/workshopmong/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmong/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nelioalves.workshopmong.domain.Post;
 import com.nelioalves.workshopmong.domain.User;
 import com.nelioalves.workshopmong.dto.UserDTO;
 import com.nelioalves.workshopmong.services.UserService;
@@ -39,6 +40,12 @@ public class UserResource {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	@PostMapping
