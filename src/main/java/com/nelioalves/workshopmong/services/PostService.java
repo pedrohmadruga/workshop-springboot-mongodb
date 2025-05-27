6fcf5dcd8fab544d47c2ac683ef345a4bfe61b0b
Pedro Madruga <pedrohfonseca01@gmail.com>
2025-05-27 13:42:52 -0300

    Simple search with query methods

--- src/main/java/com/nelioalves/workshopmong/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmong/services/PostService.java
@@ -1,12 +1,12 @@
 package com.nelioalves.workshopmong.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmong.domain.Post;
-import com.nelioalves.workshopmong.domain.User;
 import com.nelioalves.workshopmong.repository.PostRepository;
 import com.nelioalves.workshopmong.services.exceptions.ObjectNotFoundException;
 
@@ -18,5 +18,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoringCase(text);
 	}
 }
