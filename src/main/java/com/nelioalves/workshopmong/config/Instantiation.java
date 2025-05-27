b3d150603d88b2eda3e6fdb0f6408839aab7b1ed
Pedro Madruga <pedrohfonseca01@gmail.com>
2025-05-27 10:47:50 -0300

    Referencing User posts

--- src/main/java/com/nelioalves/workshopmong/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmong/config/Instantiation.java
@@ -40,5 +40,8 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2025"), "Good morning", "I woke up happy today!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 }
